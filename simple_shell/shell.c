bdae39dbe10362c32c675d901dfa529fd2165f9f
Myles2005 <cyrile450@gmail.com>
2022-08-28 13:09:01 -0700

    update

---
+++ simple_shell/shell.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+int main(void)
+{
+	pid_t pid;
+
+	pid = fork();
+
+	if (pid == -1)
+	{
+		perror("Unsuccessful\n");
+		return (-1);
+	}
+
+	if (pid == 0)
+	{
+		printf("child process\n");
+	}else
+	{
+		wait(NULL);
+		sleep(30);
+		printf("parent proces\n");
+	}
+
+	return (0);
+}
